72361e70da2a49e4f1094a7ee4dfca729a9ed34b
Kean Cabigao <93336313+kfcabigao1@users.noreply.github.com>
2025-10-09 17:00:48 +0800

    Update Export_clipped_data_CHIRPS.R

--- Export_clipped_data_CHIRPS.R
+++ Export_clipped_data_CHIRPS.R
@@ -1,63 +1,21 @@
-## This file deletes other chirps data not is not within the boundary watershed of choice
+## Automatically remove unwanted stations in your PCP directory, since it contains all pseudostations from Asia, Part 2 data.
 
-#load library
-
-library(tidyverse)
-
-setwd("C:\\Users\\E-HydroLab\\Documents\\CIA_data\\PML")
-
-## read 2 dfs containing full list of stations and selected stations
-
-data_full <- read_csv("PH_CHIRPS-CHIRTS.csv")
-dim(data_full)
-
-data_selected <- read_csv("stationsfile_PML.csv")
-dim(data_selected)
-
-## convert data selected into list
-
-stations <- data_full |> 
-  filter(!NAME %in%data_selected$NAME)
-
-dim(stations)
-
-write.csv(stations, 'removed_stations.csv')
-
-## remove files in pcp
+## Set directory to where your PCP folder is located
 
 setwd("C:/Users/E-HydroLab/Documents/CIA_data/PML/pcp")
 
   ## load df containing list of clipped chirps
 
-  df <- read_csv('removed_stations.csv') %>% 
+  df <- read_csv('removed_stations.csv') %>%  ## This CSV reads the produced list of stations that are not within your study site
   select(NAME) |> 
   mutate(NAME = paste0(NAME,".txt"))
 
-  View(df)
-
-  # convert into list
+  # convert into a list
 
   removed_stations <- as.list(df)
 
   # remove
-  sapply(removed_stations, file.remove)
-  
-## remove files in tmp
-  
-  setwd("C:/Users/E-HydroLab/Documents/CIA_data/PML/tmp")
-  
-  ## load df containing list of clipped chirps
-  
-  df <- read_csv('removed_stations.csv') %>% 
-    select(NAME) |> 
-    mutate(NAME = paste0(NAME,".txt"))
-  
-  View(df)
-  
-  # convert into list
-  
-  removed_stations <- as.list(df)
-  
-  # remove
-  sapply(removed_stations, file.remove)
+  sapply(removed_stations, file.remove) ## This line will automatically delete all .txt files containing stations that are NOT within your study
+                                        ## This line will retain all stations WITHIN your study site 
+
   
